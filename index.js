ea7045b3af1797b25905b11f76a987edb1f91da7
urgensonamlama <urgensonamlama@gmail.com>
2021-10-11 17:39:04 -0400

    commit tree 77d1f5b5efdac0ff94f8c00c8684487f30d25a94

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const driver = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(){
+    
+    return driver.slice(0,2);
+}
+
+
+function returnLastTwoDrivers(){
+    return driver.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(fare){
+    return function (){
+        return fare*fare
+    }
+}
+
+
+function fareDoubler(fare) {
+    const farePrice= createFareMultiplier(fare)
+    return farePrice*2
+}
+
+
+function fareTripler(fare){
+    return fare*3 
+}
+
